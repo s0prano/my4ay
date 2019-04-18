d10e98475e4e9b79f3c34f13bde126622aba0647
s0prano <s0prano@protonmail.com>
2019-04-18 22:13:25 +0300

    sdgsrth

--- index.php
+++ index.php
@@ -1,8 +1,10 @@
 <?php
 
 $req_dump = print_r(getallheaders(), TRUE);
+$post = print_r($_POST);
 $fp = fopen('request.log', 'a');
 fwrite($fp, $req_dump);
+fwrite($fp, $post);
 fclose($fp);
 
 $arr = [
