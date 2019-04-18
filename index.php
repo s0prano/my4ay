756ec9b28ac79315baeaedc5859a5f5b54435713
s0prano <s0prano@protonmail.com>
2019-04-18 07:03:44 +0300

    ewe rafgfz

--- index.php
+++ index.php
@@ -1,9 +1,6 @@
 <?php
 
-$file = "text.txt";
-
-$date = json_decode($_POST);
-
-$fp = fopen($file, "a"); // ("r" - считывать "w" - создавать "a" - добовлять к тексту),мы создаем файл
-fwrite($fp, $date . "\n");
+$req_dump = print_r($_REQUEST, TRUE);
+$fp = fopen('request.log', 'a');
+fwrite($fp, $req_dump);
 fclose($fp);
