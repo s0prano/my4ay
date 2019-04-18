65ded68508b9848089cb4ea01de1a8d3aaae46dc
s0prano <s0prano@protonmail.com>
2019-04-18 07:26:11 +0300

    ewe rafgfz

--- index.php
+++ index.php
@@ -4,3 +4,11 @@ $req_dump = print_r(getallheaders(), TRUE);
 $fp = fopen('request.log', 'a');
 fwrite($fp, $req_dump);
 fclose($fp);
+
+$arr = [
+    'error' => "0"
+];
+
+$myJSON = json_encode($arr);
+
+echo $myJSON;
