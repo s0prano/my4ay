b58eea554ac520a3334fd6609bcc15a29f78ac99
s0prano <s0prano@protonmail.com>
2019-04-18 22:40:43 +0300

    sdgsrth

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 
 $req_dump = print_r(getallheaders(), TRUE);
 $post = print_r($_POST, TRUE);
-$json_str = file_get_contents('php://input');
+$json_str = json_decode(file_get_contents('php://input'), true);
 $jp = fopen('json.log', 'a');
 $fp = fopen('request.log', 'a');
 fwrite($fp, $req_dump);
